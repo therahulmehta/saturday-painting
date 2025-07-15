cb4f41129d1450a22a5d1826bba141dee86e1ca4
Rahul <rahul95mehta@gmail.com>
2025-07-15 19:40:51 -0400

    Refactor sketch.js to improve grid creation and formatting; adjust point count and enhance color selection.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const { color } = require("canvas-sketch-util");
 // linear interpolation function
-const {lerp} = require("canvas-sketch-util/math");
+const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
@@ -10,20 +10,22 @@ const settings = {
 };
 
 const sketch = () => {
-  const palette = random.pick(palettes).slice(0, 2);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, 2);
   const createGrid = (rows, cols) => {
     const points = [];
-    const count = 50;
+    const count = 90;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         // normalize the x and y coordinates
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.04;
+
         points.push({
-          radius: Math.abs(random.gaussian() * 8),
+          color: random.pick(palette),
+          radius,
           position: [u, v],
-            color: random.pick(palette),
         });
       }
     }
@@ -32,7 +34,7 @@ const sketch = () => {
 
   // deterministic random from random canvas-sketch-util
   // random.setSeed('grid-dots');
-  const points = createGrid().filter(()=> random.value() > 0.5);
+  const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 200;
 
   return ({ context, width, height }) => {
@@ -40,17 +42,24 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     points.forEach((data) => {
-      const { position: [u, v], radius, color } = data;
+      const {
+        position: [u, v],
+        radius,
+        color,
+      } = data;
       // convert normalized coordinates to canvas coordinates
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius, 0, Math.PI * 2, false);
-      context.strokeStyle = "black";
-      context.lineWidth = 1;
+      // context.beginPath();
+      // context.arc(x, y, radius, 0, Math.PI * 2, false);
+      // context.strokeStyle = "black";
+      // context.lineWidth = 1;
+
       context.fillStyle = color;
-      context.fill();
+      context.font= `${radius*width}px Helvetica`;
+      context.rotate(1); 
+      context.fillText("######################################################", x, y);      
     });
   };
 };
